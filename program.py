ac0c533ba053e1d1a75c15d953188fc59887fde9
PabloHurtadoA01710778 <111140061+PabloHurtadoA01710778@users.noreply.github.com>
2022-09-30 14:07:19 -0500

    Subo Avance 6

--- program.py
+++ program.py
@@ -29,10 +29,15 @@ def asignar_palabra (palabra_escogida):
         longitud_palabra = len(palabra_escogida)
     return (lista_pistas, longitud_palabra)
 
-def prints_inicio (palabra_escogida, longitud_palabra, lista_pistas, vidas):
-    print ("Tu Palabra Es: ", palabra_escogida)
-    print ("Tu Palabra Tiene:", longitud_palabra, " letras", "_ " * longitud_palabra)
-    print ("Tus Pista Es: ", lista_pistas[0])
+def asigna_casillas (palabra_escogida, letras_correctas):
+    for let in palabra_escogida:
+        letras_correctas.append("_")
+    return letras_correctas
+
+def prints_inicio (palabra_escogida, longitud_palabra, letras_correctas, lista_pistas, vidas):
+    print ("- - - - - - - - - - - - - - - - - - - - -")
+    print ("Tu Palabra Tiene:", longitud_palabra, "letras", letras_correctas)
+    print ("Tu Pista 1 Es: ", lista_pistas[0])
     print ("Tienes: ",vidas, "Vidas")
 
 #La funcion comprueba si el usuario ingreso más de una palabra por error
@@ -54,6 +59,21 @@ def letra_si_esta (palabra_escogida, letra_usuario):
     if (letra_usuario in palabra_escogida):
         return True
     
+def poner_letra_correcta (palabra_escogida, letras_correctas, letra_usuario):
+    palabra_escogida_lista = []
+    for let in palabra_escogida:
+        palabra_escogida_lista.append(let)
+    i = 0
+    while i < len(palabra_escogida_lista):
+        if (palabra_escogida_lista[i] == letra_usuario):
+            letras_correctas[i] = palabra_escogida_lista[i]
+        i = i + 1
+    return letras_correctas
+
+def poner_letra_incorrecta (letras_erroneas, letra_usuario):
+    letras_erroneas.append(letra_usuario)
+    return letras_erroneas
+
 def eliminar_letra  (palabra_escogida_destruible, letra_usuario):
     for x in range (len(palabra_escogida_destruible)):
         palabra_escogida_destruible = palabra_escogida_destruible.replace(letra_usuario, "")
@@ -68,16 +88,20 @@ adivinado = False
 pregunta_jugar = False
 letra_no_usada = True
 comp_jugar = False
-vidas = 3
+perdio = False
+vidas = 4
 letras_usadas = []
+letras_erroneas = []
+letras_correctas = []
 lista_palabras = ["perro", "pato","piano", "fruta", "flor"]
 
 
 palabra_escogida = palabra_lazar (lista_palabras)
+lista_correctas = asigna_casillas (palabra_escogida, letras_correctas)
 lista_pistas, longitud_palabra = asignar_palabra(palabra_escogida)
 
-#Comprobacion de las palabras Con proposito de Debugeo
-prints_inicio (palabra_escogida, longitud_palabra, lista_pistas, vidas)
+#Primeros prints
+prints_inicio (palabra_escogida, longitud_palabra, letras_correctas, lista_pistas, vidas)
 #Esta parte se usa para que el usuario meta una letra, guarda la letra y comprueba si la letra ya esta en uso
 
 palabra_escogida_destruible = palabra_escogida
@@ -87,59 +111,89 @@ while (adivinado == False):
     
     
     while (valido == False):
-        letra_usuario = input("ingresa una letra profavor: ")
+        letra_usuario = input("Ingresa una letra profavor: ")
         letra_valida = validacion(letra_usuario)
-        
-        
+        print("- - - - - - - - - - - - - - - - - - - - -")       
         
         if letra_valida:
-            print("Valor valido!!!")
             letra_no_usada = lertra_usada_comprobacion(letra_usuario, letras_usadas)
             
             
             
-            if letra_no_usada:
-                print (letras_usadas)
-                print("Esa letra No Estaba Usada")
-                
+            if letra_no_usada:       
                 
                 
                 if (letra_si_esta (palabra_escogida_destruible, letra_usuario)):
-                    print ("la letra estaba en la palabra")
+                    
+                    letras_correctas = poner_letra_correcta (palabra_escogida, letras_correctas, letra_usuario)
+                    print ("Tus letras Correctas Son: ",letras_correctas)
+                    print ("Tus letras erroneas son: ", letras_erroneas)
+                    print ("la letra ", letra_usuario," estaba en la palabra")
                     palabra_escogida_destruible = eliminar_letra (palabra_escogida_destruible, letra_usuario)
-                    print (palabra_escogida_destruible)
+                    print("- - - - - - - - - - - - - - - - - - - - -")  
                     valido = False
                     
                 else:
-                    print ("Esta letra no estaba en la palabra")
+                    letras_erroneas = poner_letra_incorrecta (letras_erroneas, letra_usuario)
+                    print (letra_usuario," no estaba en la palabra, ", end = "")
+                    print ("Tus letras Correctas Son: ",letras_correctas)
+                    print ("Tus letras erroneas son: ", letras_erroneas)
                     print ("Pierdes una Vida")
                     vidas = vidas - 1
                     print ("Te quedan: ", vidas, "Vidas")
-                    print (lista_pistas[(3-vidas)])
-                    valido = False
+            
+                    if (vidas == 0):
+                        print("- - - - - - - - - - - - - - - - - - - - -") 
+                        print ("Ya no te quedan vidas")
+                        print ("Tu palabra era: ", palabra_escogida)
+                        palabra_escogida_destruible = ""
+                        perdio = True
+                    else:
+                        print ("Pista", 5-vidas, ":" ,lista_pistas[(4-vidas)])
+                        print("- - - - - - - - - - - - - - - - - - - - -")  
+                        valido = False
                 
                 
                 
                 if (palabra_escogida_destruible == ""):
+                    
+                    if (perdio == True):
+                        print ("Perdiste, Fin Del Juego")
+                    else:
+                        print ("Adivinaste la Palabra!!!")
+                        
                     lista_palabras.remove(palabra_escogida)
                     comp_jugar = False
-                    print (lista_palabras)
                     
-                    print ("Adivinaste la Palabra!!!")
                     if (lista_palabras == []):
                         print ("Acabaste Todas Las Palabras")
+                        comp_jugar = True
+                        valido = True
+                        adivinado = True
                     else:
+    
                         print ("Quieres Jugar Otra Vez?")
                         print ("Escribe: si o no")
                         while (comp_jugar == False):
                             jugar = input("")
                             if (jugar == "si" or jugar == "Si"):
                                 print ("A Jugar")
-                                palabra_escogida = palabra_lazar (lista_palabras)
-                                lista_pistas, longitud_palabra = asignar_palabra(palabra_escogida)
-                                prints_inicio (palabra_escogida, longitud_palabra, lista_pistas, vidas)
+                                
+                                #Reseteamos los valores y mandamos los prints iniciales
+                                vidas = 4
                                 letras_usadas = []
+                                letras_erroneas = []
+                                letras_correctas = []
+                                
+                                palabra_escogida = palabra_lazar (lista_palabras)
+                                lista_correctas = asigna_casillas (palabra_escogida, letras_correctas)
+                                lista_pistas, longitud_palabra = asignar_palabra(palabra_escogida)
+                                
+                                prints_inicio (palabra_escogida, longitud_palabra, letras_correctas, lista_pistas, vidas)
+                                
                                 palabra_escogida_destruible = palabra_escogida
+                                
+                                perdio = False
                                 comp_jugar = True
                                 valido = False
                             
@@ -162,5 +216,8 @@ while (adivinado == False):
             valido = False
             print("Valor No Valido, Ingresa Una Letra A La Vez")
 
-"""Los imprime para comprobar si los datos son correctos,
-en la version final esto sera elimiado"""
+"""
+Los Valores que se necesitan para debugear como: lista de palabras, la palabra
+que se esta adivinando u otras palabras importantes para probar; fueron eliminadas
+desde esta version para empezar a disenar la interfaz para el usuario.
+"""
